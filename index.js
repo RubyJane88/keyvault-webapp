5981d7443ba6468abaac17bdf80ee26fa9f6ad68
Ruby Jane <rubyjane88@gmail.com>
2025-05-28 10:14:47 +0200

    updated invalid secret name for demo

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", async (req, res) => {
   try {
     const credential = new DefaultAzureCredential();
     const client = new SecretClient(vaultUrl, credential);
-    const secret = await client.getSecret("InvalidDSecret");
+    const secret = await client.getSecret("DemoSecret001");
     res.send(`<h1>Secure Web App </h1><p>API Key: ${secret.value}</p>`);
   } catch (error) {
     res.status(500).send(`Error retrieving secret: ${error.message}`);
